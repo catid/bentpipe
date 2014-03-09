65479710da4b30bb88f01f0483e757c955bc9e6c
Chris A. Taylor <chris@gameclosure.com>
2014-03-09 15:38:17 -0700

    Add rebroadcasting

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 #include "Sockets.hpp"
@@ -7,7 +8,59 @@ using namespace cat;
 
 #include <sys/socket.h>
 
-int main() {
+struct Connexion
+{
+	NetAddr addr;
+
+	// TODO: Shorthair
+};
+
+static vector<Connexion*> m_conns;
+static SocketHandle m_s;
+
+static Connexion *findConn(const NetAddr addr)
+{
+	// TODO: Hash table
+	for (int ii = 0; ii < m_conns.size(); ++ii) {
+		Connexion *conn = m_conns[ii];
+
+		if (conn->addr == addr) {
+			return conn;
+		}
+	}
+
+	return 0;
+}
+
+static void send_data(Connexion *conn, const char *data, int len)
+{
+	// TODO: Repack data here
+
+	NetAddr::SockAddr addr_out;
+	socklen_t addrlen_out;
+
+	CAT_ENFORCE(conn->addr.Unwrap(addr_out, addrlen_out));
+
+	int result = sendto(m_s, data, len, 0, (sockaddr*)&addr_out, addrlen_out);
+
+	CAT_ENFORCE(result == len);
+}
+
+static void on_data(Connexion *conn, const char *data, int len)
+{
+	// TODO: Unpack data here
+
+	for (int ii = 0; ii < m_conns.size(); ++ii) {
+		Connexion *other = m_conns[ii];
+
+		if (conn != other) {
+			send_data(other, data, len);
+		}
+	}
+}
+
+int main()
+{
 	UDPSocket s;
 
 	CAT_ENFORCE(s.Create());
@@ -16,28 +69,41 @@ int main() {
 	CAT_ENFORCE(s.DontFragment());
 	CAT_ENFORCE(s.IgnoreUnreachable());
 
-	SocketHandle sh = s.GetSocket();
+	m_s = s.GetSocket();
 
-	char buffer[1500];
+	char buffer[2048];
 
 	for (;;) {
 		sockaddr_in6 addr;
 		socklen_t addrlen = sizeof(addr);
 
-		int result = recvfrom(sh, buffer, sizeof(buffer), 0, (sockaddr*)&addr, &addrlen);
+		int len = recvfrom(m_s, buffer, sizeof(buffer), 0, (sockaddr*)&addr, &addrlen);
 
-		if (result == -1) {
-			cout << "++ Socket error.  Giving up!" << endl;
+		if (len < 0) {
+			cout << "++ Socket error " << Sockets::GetLastErrorString() << ".  Giving up!" << endl;
 			break;
 		}
 
 		NetAddr src_addr(addr);
 		CAT_ENFORCE(src_addr.Valid());
 
-		char ipname[50];
-		const char *hostname = src_addr.IPToString(ipname, sizeof(ipname));
+		Connexion *conn = findConn(src_addr);
 
-		cout << "UDP datagram from " << hostname << " : " << src_addr.GetPort() << " of length " << result << endl;
+		if (!conn) {
+			conn = new Connexion;
+			conn->addr = addr;
+
+			m_conns.push_back(conn);
+
+			char ipname[50];
+			cout << "+ New user: " << src_addr.IPToString(ipname, sizeof(ipname)) << " : " << src_addr.GetPort() << endl;
+		}
+
+		on_data(conn, buffer, len);
 	}
+
+	cout << "Good bye." << endl;
+
+	return 0;
 }
 
